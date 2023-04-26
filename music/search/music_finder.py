5ccbfb680255c3c314a36f2d8a7d50488d90fc3c
David Manolitsas <davidmanolitsas@icloud.com>
2023-04-26 13:24:35 +1000

    refactored itunes_api_util.py

--- music/search/music_finder.py
+++ music/search/music_finder.py
@@ -1,8 +1,8 @@
+from datetime import datetime, timedelta
 from music.util.date_util import format_date, get_start_date
 from music.util.itunes_api_util import (
-    get_artists_music,
-    get_new_releases,
-    search_for_itunes_artist,
+    get_music_by_artist,
+    get_artist_by_name,
 )
 from music.util.log_util import get_logger
 from music.util.template_util import get_template
@@ -10,9 +10,72 @@ from music.util.template_util import get_template
 log = get_logger()
 
 
+def __filter_new_releases(new_releases: []) -> []:
+    # filter duplicate songs to prioritise songs with music previews
+    filtered_releases = []
+
+    for new_release_1 in new_releases:
+        for new_release_2 in new_releases:
+            if new_release_1["song"] == new_release_2["song"]:
+                if "preview" in new_release_2:
+                    filtered_releases.append(new_release_2)
+                else:
+                    filtered_releases.append(new_release_1)
+                break
+
+    return filtered_releases
+
+
+def __process_music_release(music: dict) -> dict:
+    track_count = None
+    preview = music.get("previewUrl")
+    song_url = music.get("collectionViewUrl")
+
+    if "trackCensoredName" in music:
+        song_name: str = music["trackCensoredName"]
+    else:
+        song_name: str = music["collectionName"]
+        if "trackCount" in music:
+            if music["trackCount"] > 1:
+                track_count = music["trackCount"]
+
+    return {
+        "song": song_name.replace(" - Single", "")
+        .replace(" (Extended Mix)", "")
+        .replace(" - EP", ""),
+        "preview": preview,
+        "song_url": song_url,
+        "cover": str(music["artworkUrl100"]).replace("100x100", "600x600"),
+        "track_count": track_count,
+    }
+
+
+def __filter_music_by_date(music_list: [], start_date: datetime) -> []:
+    new_releases = []
+    for music in music_list:
+        if "releaseDate" in music:
+            release_date = datetime.strptime(
+                music["releaseDate"], "%Y-%m-%dT%H:%M:%SZ"
+            )
+            if (
+                start_date
+                < release_date
+                < (datetime.today() + timedelta(days=365))
+            ):
+                new_releases.append(__process_music_release(music=music))
+
+    filtered_releases = __filter_new_releases(new_releases=new_releases)
+    return [
+        i
+        for n, i in enumerate(filtered_releases)
+        if i not in filtered_releases[n + 1 :]
+    ]
+
+
 def find_new_music(days: int, artists: []):
     """
-    Find new search for a list of artists in the past given days
+    Find new search for a list of artists in the past given days.
+
     :param days: number of days to check for past releases
     :param artists: a list of search artists
     :return: None
@@ -26,11 +89,10 @@ def find_new_music(days: int, artists: []):
     song_count = 0
 
     for artist_name in artists:
-        artist = search_for_itunes_artist(artist_name=artist_name)
+        artist = get_artist_by_name(artist_name=artist_name)
         if artist:
-
-            music = get_artists_music(artist=artist)
-            new_releases = get_new_releases(
+            music = get_music_by_artist(artist=artist)
+            new_releases = __filter_music_by_date(
                 music_list=music, start_date=start_date
             )
 
--- music/util/itunes_api_util.py
+++ music/util/itunes_api_util.py
@@ -1,71 +1,13 @@
-from datetime import datetime, timedelta
-
 import requests
 
 
-def __filter_new_releases(new_releases: []) -> []:
-    # filter duplicate songs to prioritise songs with music previews
-    filtered_releases = []
+def get_artist_by_name(artist_name: str) -> dict:
+    """
+    Query itunes api to retrieve artist by artist name
 
-    for new_release_1 in new_releases:
-        for new_release_2 in new_releases:
-            if new_release_1["song"] == new_release_2["song"]:
-                if "preview" in new_release_2:
-                    filtered_releases.append(new_release_2)
-                else:
-                    filtered_releases.append(new_release_1)
-                break
-
-    return filtered_releases
-
-
-def __process_music_release(music: dict) -> dict:
-    track_count = None
-    preview = music.get("previewUrl")
-    song_url = music.get("collectionViewUrl")
-
-    if "trackCensoredName" in music:
-        song_name: str = music["trackCensoredName"]
-    else:
-        song_name: str = music["collectionName"]
-        if "trackCount" in music:
-            if music["trackCount"] > 1:
-                track_count = music["trackCount"]
-
-    return {
-        "song": song_name.replace(" - Single", "")
-        .replace(" (Extended Mix)", "")
-        .replace(" - EP", ""),
-        "preview": preview,
-        "song_url": song_url,
-        "cover": str(music["artworkUrl100"]).replace("100x100", "600x600"),
-        "track_count": track_count,
-    }
-
-
-def get_new_releases(music_list: [], start_date: datetime) -> []:
-    new_releases = []
-    for music in music_list:
-        if "releaseDate" in music:
-            release_date = datetime.strptime(
-                music["releaseDate"], "%Y-%m-%dT%H:%M:%SZ"
-            )
-            if (
-                start_date
-                < release_date
-                < (datetime.today() + timedelta(days=365))
-            ):
-                new_releases.append(__process_music_release(music=music))
-
-    filtered_releases = __filter_new_releases(new_releases=new_releases)
-    return [
-        i
-        for n, i in enumerate(filtered_releases)
-        if i not in filtered_releases[n + 1 :]
-    ]
-
-
-def search_for_itunes_artist(artist_name: str) -> dict:
+    :param artist_name: artists name
+    :return: artist
+    """
     url_query = f'term={artist_name.replace(" ", "+")}&entity=musicArtist'
 
     artist_response = requests.get(
@@ -80,7 +22,14 @@ def search_for_itunes_artist(artist_name: str) -> dict:
     return artist_response.json()["results"][0]
 
 
-def get_artists_music(artist: dict) -> []:
+def get_music_by_artist(artist: dict) -> []:
+    """
+    Query itunes api to get all music by given artist.
+
+    :param artist: itunes artist
+    :return: list of music by artist
+    """
+
     music = []
     if "amgArtistId" in artist:
         artist_id = f'amgArtistId={artist["amgArtistId"]}'
