293cb98e4a2d748ea9e76947fde5b85ffa2f9927
David Manolitsas <davidmanolitsas@icloud.com>
2023-09-26 11:44:20 +1000

    use artist id instead of artist name (#26)

--- music/api/itunes_api.py
+++ music/api/itunes_api.py
@@ -5,7 +5,7 @@ from music.util.log_util import get_logger
 log = get_logger()
 
 
-def __send_request(url: str):
+def __send_http_request(url: str):
     try:
         response = requests.get(url=url, timeout=20)
         response.raise_for_status()
@@ -15,62 +15,29 @@ def __send_request(url: str):
         return None
 
 
-def __remove_artist_details(response):
-    results = response.json().get("results")
-    del results[0]
-    return results
-
-
-def get_artist_by_name(name: str) -> dict:
-    """
-    Query itunes api to retrieve artist by artist name
-
-    :param name: artists name
-    :return: artist
-    """
-    url_query = f'term={name.replace(" ", "+")}&entity=musicArtist'
-
-    response = __send_request(
-        url=f"https://itunes.apple.com/search?{url_query}"
-    )
-
-    if response:
-        if len(response.json().get("results")) > 1:
-            for artist in response.json().get("results"):
-                if artist["artistName"] == name:
-                    return artist
-
-        return response.json().get("results")[0]
-
-
 def get_music_by_artist(artist: dict) -> []:
     """
     Query itunes api to get all music by given artist.
 
-    :param artist: itunes artist
+    :param artist: itunes artist id
     :return: list of music by artist
     """
     music = []
-
-    if "amgArtistId" in artist:
-        artist_id = f'amgArtistId={artist["amgArtistId"]}'
-    else:
-        artist_id = f'id={artist["artistId"]}'
+    artist_id = artist.get("id")
 
     # get all songs by artist
-    song_response = __send_request(
-        url=f"https://itunes.apple.com/lookup?{artist_id}&entity=song",
+    song_response = __send_http_request(
+        url=f"https://itunes.apple.com/lookup?id={artist_id}&entity=song",
     )
+
     if song_response:
-        songs = __remove_artist_details(response=song_response)
-        music.extend(songs)
+        music.extend(song_response.json().get("results"))
 
     # get all albums by artist
-    album_response = __send_request(
-        url=f"https://itunes.apple.com/lookup?{artist_id}&entity=album",
+    album_response = __send_http_request(
+        url=f"https://itunes.apple.com/lookup?id={artist_id}&entity=album",
     )
     if album_response:
-        albums = __remove_artist_details(response=album_response)
-        music.extend(albums)
+        music.extend(album_response.json().get("results"))
 
     return music
---
+++ music/search/music_filter.py
@@ -0,0 +1,78 @@
+from datetime import datetime, timedelta
+
+
+def filter_music_duplicates(new_releases: []) -> []:
+    """
+    Remove any duplicate music releases.
+
+    :param new_releases: all new music releases by artist
+    :return: filtered music releases
+    """
+    # Dictionary to keep track of seen songs
+    seen_songs = {}
+
+    for release in new_releases:
+        song = release["song"]
+        if song in seen_songs:
+            # Check if the current release has a preview
+            if "preview" in release:
+                seen_songs[song] = release
+        else:
+            seen_songs[song] = release
+
+    return list(seen_songs.values())
+
+
+def map_itunes_music_to_new_release(music: dict) -> dict:
+    """
+    Map the music config to the final artist config.
+
+    :param music: raw music data
+    :return: refined artist new release
+    """
+    preview = music.get("previewUrl")
+    song_url = music.get("collectionViewUrl")
+    song_name: str = music.get("trackCensoredName")
+
+    track_count = None
+
+    if not song_name:
+        song_name: str = music.get("collectionName")
+        if "trackCount" in music:
+            if music.get("trackCount") > 1:
+                track_count = music.get("trackCount")
+
+    return {
+        "song": song_name.replace(" - Single", "")
+        .replace(" (Extended Mix)", "")
+        .replace(" - EP", ""),
+        "preview": preview,
+        "song_url": song_url,
+        "cover": str(music["artworkUrl100"]).replace("100x100", "600x600"),
+        "track_count": track_count,
+    }
+
+
+def get_all_new_releases(music_list: [], start_date: datetime):
+    """
+    Get all new music releases unfiltered.
+
+    :param music_list: all artist music
+    :param start_date: release date cut off
+    :return: all new music by artist
+    """
+    new_releases = []
+
+    for music in music_list:
+        if "releaseDate" in music:
+            release_date = datetime.strptime(
+                music["releaseDate"], "%Y-%m-%dT%H:%M:%SZ"
+            )
+            year_from_now = datetime.today() + timedelta(days=365)
+
+            if start_date < release_date < year_from_now:
+                new_releases.append(
+                    map_itunes_music_to_new_release(music=music)
+                )
+
+    return new_releases
