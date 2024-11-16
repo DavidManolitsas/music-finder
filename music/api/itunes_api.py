557ceb1b3f8c6194311752b4c92a5a48dc555bad
David Manolitsas <davidmanolitsas@icloud.com>
2024-11-16 21:20:32 +1100

    redesign to better support mobile devices

--- music/api/itunes_api.py
+++ music/api/itunes_api.py
@@ -1,42 +1,30 @@
 import requests
 
+from music.api.http_service import HttpService
 from music.util.log_util import get_logger
 
 log = get_logger()
+service = HttpService(base_url="https://itunes.apple.com")
 
 
-def send_http_request(url: str):
-    try:
-        response = requests.get(url=url, timeout=20)
-        response.raise_for_status()
-        return response
-    except requests.exceptions.HTTPError as error:
-        log.error(f"iTunes API error {error}")
-        return None
-
-
-def get_music_by_artist(artist: dict) -> []:
+def get_music_by_artist_id(artist_id: str) -> []:
     """
     Query itunes api to get all music by given artist.
 
-    :param artist: itunes artist id
+    :param artist_id: itunes artist id
     :return: list of music by artist
     """
     music = []
-    artist_id = artist.get("id")
 
     # get all songs by artist
-    song_response = send_http_request(
-        url=f"https://itunes.apple.com/lookup?id={artist_id}&entity=song",
-    )
+    song_response = service.get(path=f"/lookup?id={artist_id}&entity=song")
 
     if song_response:
         music.extend(song_response.json().get("results"))
 
     # get all albums by artist
-    album_response = send_http_request(
-        url=f"https://itunes.apple.com/lookup?id={artist_id}&entity=album",
-    )
+    album_response = service.get(path=f"/lookup?id={artist_id}&entity=album")
+
     if album_response:
         music.extend(album_response.json().get("results"))
 
